b19ac5a887f8f4a053e24cddad7e1352149d4786
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-28 13:19:08 +0900

    2_3_add_app_title

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.jetbmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    
+                    Column(
+                        modifier = Modifier.padding(20.dp),
+                        horizontalAlignment = Alignment.Start,
+                    ) {
+                        Text(text = "BMI計算アプリ", fontSize = 26.sp, fontWeight = FontWeight.Bold)
+                    }
                 }
             }
         }
