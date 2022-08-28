d8535fbb6389a6e83ec114209e6b8c2f14972c47
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-28 18:51:04 +0900

    2_7_setup_view_model

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jetbmicalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -20,8 +22,11 @@ import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             JetBMICalculatorTheme {
                 // A surface container using the 'background' color from the theme
